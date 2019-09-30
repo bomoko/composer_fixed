e0a82eb332472e87f95794c9852de84330511a0f
= <blaize.kaye@gmail.com>
2019-09-30 14:43:28 +1300

    Saving changes

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,6 +8,9 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PreFileDownloadEvent;
+use Composer\Package\Version\VersionSelector;
+use Composer\DependencyResolver\Pool;
+use Composer\Repository\CompositeRepository;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
@@ -18,7 +21,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface
   {
     $this->composer = $composer;
     $this->io = $io;
-    echo "IM IN HERE";
   }
 
   public static function getSubscribedEvents()
@@ -29,7 +31,51 @@ class Plugin implements PluginInterface, EventSubscriberInterface
   }
 
   public function onPostUpdate() {
-    var_dump($this->composer->getPackage()->getRequires());
-    var_dump($this->composer->getRepositoryManager()->getLocalRepository()->getPackages());
+    $this->generateAbsolutePackages();
   }
+
+  public function outputFile($packagesArray) {
+    $output = [
+      "name" => "somemeta/package",
+      "type" => "metapackage",
+      "description" => "locked dependencies",
+      "require" => $packagesArray
+    ];
+    print json_encode($output);
+  }
+
+  protected function generateAbsolutePackages() {
+    $rootPackage = $this->composer->getPackage();
+    $requires = $rootPackage->getRequires();
+
+    $lockPackages = [];
+
+    foreach ($this->composer->getRepositoryManager()
+               ->getLocalRepository()
+               ->getPackages() as $package) {
+      $lockPackages[$package->getName()] = $package->getVersion();
+    }
+    $composer = $this->composer;
+    //new Pool($composer->getPackage()->getMinimumStability(), $composer->getPackage()->getStabilityFlags());
+    $pool = new Pool($composer->getPackage()->getMinimumStability(), $composer->getPackage()->getStabilityFlags());
+//    $pool->addRepository(new CompositeRepository($composer->getRepositoryManager()->getRepositories()));
+        $pool->addRepository($composer->getRepositoryManager()->getLocalRepository());
+    $vs = new VersionSelector($pool);
+
+
+    $directAbsolutePackages = [];
+    var_dump(get_class($this->composer->getPackage()));
+    foreach ($this->composer->getPackage()
+               ->getRequires() as $requireName => $require) {
+//                $directAbsolutePackages[$requi]
+      var_dump($requireName);
+
+      var_dump($vs->findBestCandidate($requireName)->getPrettyVersion());//$require->getPrettyVersion());
+//      $directAbsolutePackages[$requireName] = $lockPackages[$requireName];
+    }
+
+//    $this->outputFile($directAbsolutePackages);
+  }
+
+
 }
